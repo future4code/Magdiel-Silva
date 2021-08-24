6803d9eab115284576141939557c92f70214dc45
magdielSilva <Magdiel Silva>
2021-08-24 16:16:54 -0300

    revisao-js

--- semana-3/revisao-js/exercicios.js
+++ semana-3/revisao-js/exercicios.js
@@ -5,17 +5,17 @@
 
 // EXERCÍCIO 01
 function retornaTamanhoArray(array) {
-   return array.length
+    return array.length
 }
 
 // EXERCÍCIO 02
 function retornaArrayInvertido(array) {
-  return array.reverse()
+    return array.reverse()
 }
 
 // EXERCÍCIO 03
 function retornaArrayOrdenado(array) {
-  return array.sort((a, b) => a - b)
+    return array.sort((a, b) => a - b)
 }
 
 // EXERCÍCIO 04
@@ -26,58 +26,63 @@ function retornaNumerosPares(array) {
 
 // EXERCÍCIO 05
 function retornaNumerosParesElevadosADois(array) {
-   let retornaPares = array.filter((numero) => numero % 2 === 0)
-    for(let i = 0; i < retornaPares.length; i++){
+    let retornaPares = array.filter((numero) => numero % 2 === 0)
+    for (let i = 0; i < retornaPares.length; i++) {
         retornaPares[i] = retornaPares[i] * retornaPares[i]
     }
     return retornaPares
 }
 // EXERCÍCIO 06
 function retornaMaiorNumero(array) {
-  let i = 0
-  let maior = -Infinity
-  while (i < array.length) {
-      if (array[i] > maior){
-          maior = array[i]  
-      }
-    i = i+1
-  } 
-  return maior
+    let i = 0
+    let maior = -Infinity
+    while (i < array.length) {
+        if (array[i] > maior) {
+            maior = array[i]
+        }
+        i = i + 1
+    }
+    return maior
 }
-    
+
 // EXERCÍCIO 07
 
 function retornaObjetoEntreDoisNumeros(num1, num2) {
+    let menorNumero
 
-//     AINDA NÃO CONSEGUI ENCONTRAR UMA LÓGICA
-//         const objeto = {
-//         maiorNumero: 0,
-//         maiorDivisivelPorMenor: 0,
-//         diferenca = 0
-    
-//     }
-//     if (num1 > num2){
-//         return `maiorNumero: ${num1}`
-//  } else ( num2 > num1)
-//  {
-//         return `maiorNumero: ${num2}`
-//     } 
-// }
+    const objeto = {
+        maiorNumero: 0,
+        maiorDivisivelPorMenor: 0,
+        diferenca: 0
+    }
+    if (num1 > num2) {
+        objeto.maiorNumero = num1
+        menorNumero = num2
+    } else {
+        objeto.maiorNumero = num2
+        menorNumero = num1
+    }
+
+    objeto.maiorDivisivelPorMenor = objeto.maiorNumero % menorNumero === 0
+    objeto.diferenca = objeto.maiorNumero - menorNumero
+    return objeto
+}
+
+
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   // AINDA NÃO CONSEGUI ENCONTRAR UMA LÓGICA
 }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
 
     if (ladoA === ladoB === ladoC) {
-        return "equilátero"
+        return "Equilátero"
     } else if (ladoA !== ladoB !== ladoC) {
-        return "retângula ou escaleno"
+        return "Escaleno"
     } else {
-        return "isósceles"
+        return "Isósceles"
     }
 
 }
@@ -89,28 +94,28 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-    return `venha assistir ao filme ${filme.nome}, de ${filme.ano},
+    return `Venha assistir ao filme ${filme.nome}, de ${filme.ano},
     dirigido por ${filme.diretor} e estrelado por ${filme.atores} `
-   
+
 }
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
-   const novoObjeto = {
-       ...pessoa,
-       nome: "Anônimo"
+    const novoObjeto = {
+        ...pessoa,
+        nome: "ANÔNIMO"
     }
     return novoObjeto
 }
 
 // EXERCÍCIO 13A
 function retornaPessoasAutorizadas(pessoas) {
-   
+
 }
 
 // EXERCÍCIO 13B
 function retornaPessoasNaoAutorizadas(pessoas) {
-  
+
 }
 
 // EXERCÍCIO 14
@@ -120,7 +125,7 @@ function retornaContasComSaldoAtualizado(contas) {
 
 // EXERCÍCIO 15A
 function retornaArrayOrdenadoAlfabeticamente(consultas) {
-  
+
 }
 
 // EXERCÍCIO 15B
