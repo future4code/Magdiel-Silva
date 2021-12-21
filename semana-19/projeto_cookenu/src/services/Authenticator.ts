755b85a6e080d5a42a73a66520ba1ae0a4c0ccf7
magdielSilva <arq.magdielsilva@gmail.com>
2021-12-21 14:40:47 -0300

    authenticator ajuste

--- semana-19/projeto_cookenu/src/services/Authenticator.ts
+++ semana-19/projeto_cookenu/src/services/Authenticator.ts
@@ -4,12 +4,16 @@ export interface AuthenticationData {
     id: string
 }
 
-
 export class Authenticator {
     public generate = (input: AuthenticationData):string => {
         const token = jwt.sign(input, process.env.KWT_KEY as string, {
             expiresIn:process.env.ACCESS_TOKEN_EXPIRES_IN
         });
         return token
+    }
+
+    public getTokenData = (token: string):AuthenticationData => {
+        const data = jwt.verify(token, process.env.JWT_KEY as string)
+        return data as AuthenticationData
     }
 }
