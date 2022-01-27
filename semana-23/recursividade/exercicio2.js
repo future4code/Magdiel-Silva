be8b36dd2be4da66fc4952868c42216dabd87ee3
magdielSilva <arq.magdielsilva@gmail.com>
2022-01-27 14:30:36 -0300

    exercicio-2

---
+++ semana-23/recursividade/exercicio2.js
@@ -0,0 +1,11 @@
+//EXERCICIO 2
+// Escreva uma função recursiva que calcule a soma dos números inteiros
+// de 0 a n
+var calculaSoma = function (n, soma) {
+    if (soma === void 0) { soma = 0; }
+    if (n === 0) {
+        return soma;
+    }
+    return calculaSoma(n - 1, soma + n);
+};
+console.log(calculaSoma(10));
--- semana-23/recursividade/exercicio2.ts
+++ semana-23/recursividade/exercicio2.ts
@@ -3,9 +3,12 @@
 // Escreva uma função recursiva que calcule a soma dos números inteiros
 // de 0 a n
 
-const calculaSoma = (n:number):number|string => {
-    if(n <= 0){
-        return "Insita um número maior que zero"
+const calculaSoma = (n:number, soma:number = 0):number|string => {
+    if(n === 0){
+        return soma
     }
+       return calculaSoma(n - 1, soma + n)
     
 }
+console.log(calculaSoma(10));
+  
